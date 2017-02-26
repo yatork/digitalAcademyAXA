27633a1d2cd1c2c2179e8bd2f621e9065f49cbce
Yacov Hamou <yacovhamou@gmail.com>
2017-02-26 23:42:19 +0100

    sort_last + test

--- FirstPartExercise.py
+++ FirstPartExercise.py
@@ -22,3 +22,39 @@ def front_x(words):
     list1 = sorted(list1)
     list2 = sorted(list2)
     return list2 + list1
+
+
+def get_last_tupple_element(thetuples):
+    return thetuples[len(thetuples)-1]
+
+def sort_last(tuples):
+    return sorted(tuples,key = get_last_tupple_element)
+
+def test(got, expected):
+    prefix = 'OK' if got == expected else ' X'
+    # !r prints a Python representation of the strings (complete with quotes)
+    print ' {} got: {!r} expected: {!r}'.format(prefix, got, expected)
+
+def main():
+    print 'match_ends'
+    test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+    test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+    test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+    print
+    print 'front_x'
+    test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+        ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+    test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+        ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+    test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+        ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+    print
+    print 'sort_last'
+    test(sort_last([(1, 3), (3, 2), (2, 1)]),
+         [(2, 1), (3, 2), (1, 3)])
+    test(sort_last([(2, 3), (1, 2), (3, 1)]),
+         [(3, 1), (1, 2), (2, 3)])
+    test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+         [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
