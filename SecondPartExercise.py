eb16352424f42d4a451b136c116c9aec7a84c7a6
Yacov Hamou <yacovhamou@gmail.com>
2017-02-28 11:39:02 +0100

    fin exercices préparatoires

--- SecondPartExercise.py
+++ SecondPartExercise.py
@@ -91,3 +91,58 @@ def main():
     test(mix_up('dog', 'dinner'), 'dig donner')
     test(mix_up('gnash', 'sport'), 'spash gnort')
     test(mix_up('pezzy', 'firm'), 'fizzy perm')
+
+
+
+def verbing(s):
+    len_s = len(s)
+    if len_s > 2:
+        if s[len_s-3:] == "ing":
+            s = s+"ly"
+        else:
+            s = s+"ing"
+    return s
+
+def not_bad(s):
+    myString = s.split(" ")
+    activate_remove = False
+    i = 0
+    index_not = 0
+    index_bad = 0
+    while i < len(myString):
+        if myString[i] == "not":
+            index_not = i
+            activate_remove = True
+        elif myString[i][0:3] == "bad" and activate_remove == True:
+            index_bad = i
+            myString[i]="good"+ myString[i][3:]
+            myString = myString[0:index_not] + myString[index_bad:]
+            break
+        i+=1
+    myString = ' '.join(str(e) for e in myString)
+    return myString
+
+
+def front_back(a, b):
+    half_a = len(a)/2+1 if len(a) % 2 == 1 else len(a)/2
+    half_b = len(b)/2+1 if len(b) % 2 == 1 else len(b)/2
+    return a[0:half_a] + b[0:half_b] + a[half_a:] + b[half_b:]
+
+def main():
+    print 'verbing'
+    test(verbing('hail'), 'hailing')
+    test(verbing('swiming'), 'swimingly')
+    test(verbing('do'), 'do')
+
+    print
+    print 'not_bad'
+    test(not_bad('This movie is not so bad'), 'This movie is good')
+    test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
+    test(not_bad('This tea is not hot'), 'This tea is not hot')
+    test(not_bad("It's bad yet not"), "It's bad yet not")
+
+    print
+    print 'front_back'
+    test(front_back('abcd', 'xy'), 'abxcdy')
+    test(front_back('abcde', 'xyz'), 'abcxydez')
+    test(front_back('Kitten', 'Donut'), 'KitDontenut')
